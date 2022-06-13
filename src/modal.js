860928a2b7d1d73fe793f3727a8bf1bcadc80bea
germang13 <german.gar13@gmail.com>
2022-06-13 08:27:41 -0300

    creo primer seccion en index.html

--- src/modal.js
+++ src/modal.js
@@ -2,3 +2,8 @@ const openMOdal = () => {
   const modal = document.getElementById("modal");
   modal.style.display = "flex";
 };
+
+const close = () => {
+  const modal = document.getElementById("modal");
+  modal.style.display = "none";
+};
